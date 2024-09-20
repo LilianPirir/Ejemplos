9233883109e87f714cc23283634e93a4eb0ec0b0
Lilian Pirir <lilian@gmail.com>
2024-09-19 23:28:08 -0600

    primer commit

---
+++ EjercicioPOO/src/ejercicioPoo/ProductoElectronico.java
@@ -0,0 +1,31 @@
+package ejercicioPoo;
+public abstract class ProductoElectronico {
+    private double precio;
+    private String marca;
+    private String modelo;
+
+  
+    public double getPrecio() {
+        return precio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
